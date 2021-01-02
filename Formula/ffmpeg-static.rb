d7d4a01ea487651b5274190ea632fbd064938ac3
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-02 15:18:46 +0100

    rename

--- Formula/ffmpeg-static.rb
+++ Formula/ffmpeg-static.rb
@@ -1,10 +1,9 @@
-class Ffmpeg < Formula
+class FfmpegStatic < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
   # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
   # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   license "GPL-2.0-or-later"
-  revision 6
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   stable do
