0196a4b3c8e33c3a71410dc61fd1dd4b1a4b8b25
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-05 15:41:24 +0100

    add opencv-static

---
+++ Formula/opencv-static.rb
@@ -0,0 +1,110 @@
+class OpencvStatic < Formula
+  desc "Open source computer vision library"
+  homepage "https://opencv.org/"
+  url "https://github.com/opencv/opencv/archive/4.5.0.tar.gz"
+  sha256 "dde4bf8d6639a5d3fe34d5515eab4a15669ded609a1d622350c7ff20dace1907"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "eigen"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "tbb"
+  depends_on "webp"
+
+  resource "contrib" do
+    url "https://github.com/opencv/opencv_contrib/archive/4.5.0.tar.gz"
+    sha256 "a65f1f0b98b2c720abbf122c502044d11f427a43212d85d8d2402d7a6339edda"
+  end
+
+  def install
+    ENV.cxx11
+
+    resource("contrib").stage buildpath/"opencv_contrib"
+
+    # Avoid Accelerate.framework
+    ENV["OpenBLAS_HOME"] = Formula["openblas"].opt_prefix
+
+    # Reset PYTHONPATH, workaround for https://github.com/Homebrew/homebrew-science/pull/4885
+    ENV.delete("PYTHONPATH")
+
+    args = std_cmake_args + %W[
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=
+      -DBUILD_EXAMPLES=OFF
+      -DBUILD_opencv_apps=OFF
+      -DBUILD_JASPER=OFF
+      -DBUILD_ZLIB=OFF
+      -DBUILD_JPEG=OFF
+      -DBUILD_WEBP=OFF
+      -DBUILD_OPENEXR=OFF
+      -DBUILD_PERF_TESTS=OFF
+      -DBUILD_PNG=OFF
+      -DBUILD_TESTS=OFF
+      -DBUILD_TIFF=OFF
+      -DBUILD_opencv_hdf=OFF
+      -DBUILD_opencv_java=OFF
+      -DBUILD_opencv_text=OFF
+      -DOPENCV_ENABLE_NONFREE=ON
+      -DOPENCV_EXTRA_MODULES_PATH=#{buildpath}/opencv_contrib/modules
+      -DOPENCV_GENERATE_PKGCONFIG=ON
+      -DWITH_ITT=OFF
+      -DWITH_XINE=OFF
+      -DWITH_1394=OFF
+      -DWITH_CUDA=OFF
+      -DWITH_EIGEN=ON
+      -DWITH_FFMPEG=OFF
+      -DWITH_GPHOTO2=OFF
+      -DWITH_GSTREAMER=OFF
+      -DWITH_JASPER=OFF
+      -DWITH_OPENEXR=OFF
+      -DWITH_OPENGL=OFF
+      -DWITH_QT=OFF
+      -DWITH_TBB=ON
+      -DWITH_VTK=OFF
+      -DWITH_PROTOBUF=OFF
+      -DWITH_QUIRC=OFF
+      -DWITH_ADE=OFF
+      -DWITH_IPP=OFF
+      -DBUILD_opencv_python2=OFF
+      -DBUILD_opencv_python3=OFF
+    ]
+
+    if Hardware::CPU.intel?
+      args << "-DENABLE_AVX=OFF" << "-DENABLE_AVX2=OFF"
+      args << "-DENABLE_SSE41=OFF" << "-DENABLE_SSE42=OFF" unless MacOS.version.requires_sse42?
+    end
+
+    mkdir "build" do
+      system "cmake", "..", "-DBUILD_SHARED_LIBS=OFF", *args
+      inreplace "modules/core/version_string.inc", "#{HOMEBREW_SHIMS_PATH}/mac/super/", ""
+      system "make"
+      system "make", "install"
+
+      # Jeroen: fix static linking flags
+      inreplace "#{lib}/pkgconfig/opencv4.pc", "-lAccelerate.framework",
+        "-framework Accelerate -framework AVFoundation"
+      # inreplace "#{lib}/pkgconfig/opencv4.pc", "-llibz.dylib", "-lz"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <opencv2/opencv.hpp>
+      #include <iostream>
+      int main() {
+        std::cout << CV_VERSION << std::endl;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp", "-I#{include}/opencv4",
+                    "-o", "test"
+    assert_equal `./test`.strip, version.to_s
+  end
+end
