f6e111f565fb86266ca3e1d76e3d71d07a720ea7
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-12 14:36:34 +0100

    add libraw w/o openmp

---
+++ Formula/libraw-lite.rb
@@ -0,0 +1,43 @@
+class LibrawLite < Formula
+  desc "Library for reading RAW files from digital photo cameras"
+  homepage "https://www.libraw.org/"
+  url "https://www.libraw.org/data/LibRaw-0.20.2.tar.gz"
+  sha256 "dc1b486c2003435733043e4e05273477326e51c3ea554c6864a4eafaff1004a6"
+  license any_of: ["LGPL-2.1-only", "CDDL-1.0"]
+
+  livecheck do
+    url "https://www.libraw.org/download/"
+    regex(/href=.*?LibRaw[._-]v?(\d+(?:\.\d+)*)\.t/i)
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "jasper"
+  depends_on "jpeg"
+  depends_on "little-cms2"
+
+  resource "librawtestfile" do
+    url "https://www.rawsamples.ch/raws/nikon/d1/RAW_NIKON_D1.NEF"
+    sha256 "7886d8b0e1257897faa7404b98fe1086ee2d95606531b6285aed83a0939b768f"
+  end
+
+  # Jeroen: removed OpenMP here
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking"
+    system "make", "install"
+    doc.install Dir["doc/*"]
+    prefix.install "samples"
+  end
+
+  test do
+    resource("librawtestfile").stage do
+      filename = "RAW_NIKON_D1.NEF"
+      system "#{bin}/raw-identify", "-u", filename
+      system "#{bin}/simple_dcraw", "-v", "-T", filename
+    end
+  end
+end
