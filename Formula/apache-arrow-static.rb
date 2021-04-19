db77ef5506b89c03520cf995c5642803638f7a60
Jonathan Keane <jkeane@gmail.com>
2021-04-19 13:56:21 -0500

    arrow 4.0.0 rc1

--- Formula/apache-arrow-static.rb
+++ Formula/apache-arrow-static.rb
@@ -1,14 +1,14 @@
 class ApacheArrowStatic < Formula
   desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
-  url "https://downloads.apache.org/arrow/arrow-3.0.0/apache-arrow-3.0.0.tar.gz"
+  # url "https://downloads.apache.org/arrow/arrow-4.0.0/apache-arrow-4.0.0.tar.gz"
   # Uncomment and update to test on a release candidate
-  # url "https://dist.apache.org/repos/dist/dev/arrow/apache-arrow-3.0.0-rc0/apache-arrow-3.0.0.tar.gz"
-  sha256 "73c2cc3be537aa1f3fd9490cfec185714168c9bfd599d23e287ab0cc0558e27a"
+  url "https://dist.apache.org/repos/dist/dev/arrow/apache-arrow-4.0.0-rc1/apache-arrow-4.0.0.tar.gz"
+  sha256 "51b05585db4f5e6ad8b5e8bd2eb9643bdc2278cf607799b041974bf354af7eb2"
   head "https://github.com/apache/arrow.git"
 
   bottle do
-    root_url "https://github.com/autobrew/homebrew-cran/releases/download/apache-arrow-static-3.0.0"
+    root_url "https://github.com/autobrew/homebrew-cran/releases/download/apache-arrow-static-4.0.0"
     sha256 cellar: :any, arm64_big_sur: "9348f39d8e10876f446f734729a25c340a888b27e1c4be9642495279633818c0"
     sha256 cellar: :any, big_sur:       "7132f09b2f65a9e14c4cabd76b2cefbb6663ec5d4b235c6469ab18b4f4a42d59"
     sha256 cellar: :any, catalina:      "f34c2242f9e2d3747ff2c1837c31285ee7c16394cf01eebe80c657095d2e82ee"
