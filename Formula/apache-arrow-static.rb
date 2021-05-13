b19c79a1415d8c7352a560d8e20e4b7597db6375
Jeroen Ooms <jeroenooms@gmail.com>
2021-05-13 11:00:16 +0200

    add arm bottle

--- Formula/apache-arrow-static.rb
+++ Formula/apache-arrow-static.rb
@@ -10,6 +10,7 @@ class ApacheArrowStatic < Formula
 
   bottle do
     root_url "https://github.com/autobrew/homebrew-cran/releases/download/apache-arrow-static-4.0.0_1"
+    sha256 cellar: :any, arm64_big_sur:  "f0880024fddc620ee57b0aae6f1d4305e34be888b9cf1a936a905f21035cba1f"
     sha256 cellar: :any, big_sur:  "55fa5b9204c8152d05ecb69f0950c0237e4456b9713909c5fbffce6351d20cc9"
     sha256 cellar: :any, catalina: "87cc2596aa3b76a2ce629f3bb18ac77c287c447592e0ea7651b3fdf5c90f8755"
   end
