11224bfd9633337ee2af4428a7524bbd2be7f6e6
Jeroen Ooms <jeroenooms@gmail.com>
2021-12-19 16:14:47 +0100

    add bottle

--- Formula/aws-sdk-cpp-static.rb
+++ Formula/aws-sdk-cpp-static.rb
@@ -8,8 +8,9 @@ class AwsSdkCppStatic < Formula
 
   bottle do
     root_url "https://github.com/autobrew/homebrew-cran/releases/download/aws-sdk-cpp-static-1.9.163"
-    sha256 cellar: :any_skip_relocation, big_sur:  "a341ae23be8bfbd70125219ff8bc52e71e6c22dbd5b630808c6bac0088ffac4e"
-    sha256 cellar: :any_skip_relocation, catalina: "4f6586822f8b61083a4dcec09a550a2bb2e018dc5804f6d84686d57e6123feaa"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2a1ecb1a4165569bbc2d8ac77f44bdb7afb94f21767be47e3411c949a76abb6b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "a341ae23be8bfbd70125219ff8bc52e71e6c22dbd5b630808c6bac0088ffac4e"
+    sha256 cellar: :any_skip_relocation, catalina:      "4f6586822f8b61083a4dcec09a550a2bb2e018dc5804f6d84686d57e6123feaa"
   end
 
   depends_on "cmake" => :build
