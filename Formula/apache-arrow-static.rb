38b85f786768b11e3e2b502b8f873d28298f152e
Jeroen Ooms <jeroenooms@gmail.com>
2022-05-04 12:20:11 +0200

    test arrow 8.0.0

--- Formula/apache-arrow-static.rb
+++ Formula/apache-arrow-static.rb
@@ -1,10 +1,10 @@
 class ApacheArrowStatic < Formula
   desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
-  url "https://downloads.apache.org/arrow/arrow-7.0.0/apache-arrow-7.0.0.tar.gz"
+  url "https://downloads.apache.org/arrow/arrow-8.0.0/apache-arrow-8.0.0.tar.gz"
   # Uncomment and update to test on a release candidate
-  # mirror "https://dist.apache.org/repos/dist/dev/arrow/apache-arrow-7.0.0-rc8/apache-arrow-7.0.0.tar.gz"
-  sha256 "e8f49b149a15ecef4e40fcfab1b87c113c6b1ee186005c169e5cdf95d31a99de"
+  mirror "https://dist.apache.org/repos/dist/dev/arrow/apache-arrow-8.0.0-rc3/apache-arrow-8.0.0.tar.gz"
+  sha256 "ad9a05705117c989c116bae9ac70492fe015050e1b80fb0e38fde4b5d863aaa3"
   head "https://github.com/apache/arrow.git"
 
   bottle do
