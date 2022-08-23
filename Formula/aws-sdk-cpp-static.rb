74af105d6d498339f1daa30c221ca83270134113
Jeroen Ooms <jeroenooms@gmail.com>
2022-08-24 00:21:20 +0200

    trigger build

--- Formula/aws-sdk-cpp-static.rb
+++ Formula/aws-sdk-cpp-static.rb
@@ -34,7 +34,6 @@ class AwsSdkCppStatic < Formula
     (testpath/"test.cpp").write <<~EOS
       #include <aws/core/Version.h>
       #include <iostream>
-
       int main() {
           std::cout << Aws::Version::GetVersionString() << std::endl;
           return 0;
