13edd37ce0f0babd46e04646db53fe1a7e6ef70d
Jeroen Ooms <jeroen@berkeley.edu>
2022-08-23 23:01:57 +0000

    aws-sdk-cpp-static: update 1.9.163 bottle.

--- Formula/aws-sdk-cpp-static.rb
+++ Formula/aws-sdk-cpp-static.rb
@@ -11,6 +11,8 @@ class AwsSdkCppStatic < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "2a1ecb1a4165569bbc2d8ac77f44bdb7afb94f21767be47e3411c949a76abb6b"
     sha256 cellar: :any_skip_relocation, big_sur:       "a341ae23be8bfbd70125219ff8bc52e71e6c22dbd5b630808c6bac0088ffac4e"
     sha256 cellar: :any_skip_relocation, catalina:      "4f6586822f8b61083a4dcec09a550a2bb2e018dc5804f6d84686d57e6123feaa"
+    sha256 cellar: :any_skip_relocation, monterey:      "7e12a0802b447ac7b3c5ee22c0ab8f362212d1cfd1e8ee613014dfba1b6224c7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "07a90b8b0831d0efb38c29a024c9791acc95b67fdd71fadbc2f4c9151386efda"
   end
 
   depends_on "cmake" => :build
