198ea8aca7363b48dbc52c3574b9ec5110acd932
Jeroen Ooms <jeroenooms@gmail.com>
2023-04-11 13:55:23 +0200

    glib

--- Formula/glib-lite.rb
+++ Formula/glib-lite.rb
@@ -3,71 +3,71 @@ class GlibLite < Formula
 
   desc "Core application library for C"
   homepage "https://developer.gnome.org/glib/"
-  url "https://download.gnome.org/sources/glib/2.66/glib-2.66.4.tar.xz"
-  sha256 "97df8670e32f9fd4f7392b0980e661dd625012015d58350da1e58e343f4af984"
+  url "https://download.gnome.org/sources/glib/2.76/glib-2.76.1.tar.xz"
+  sha256 "43dc0f6a126958f5b454136c4398eab420249c16171a769784486e25f2fda19f"
   license "LGPL-2.1-or-later"
 
-  livecheck do
-    url :stable
-  end
-
   bottle do
-    root_url "https://github.com/autobrew/homebrew-cran/releases/download/glib-lite-2.66.4"
-    sha256 arm64_big_sur: "4359ba3bb9f7de80e5b0a4e32846965e7b948d34a8b5be426c8291d5fb0c40d4"
-    sha256 big_sur:       "36358586fa7435f04864f8e4b198e70b592758ade611159aca95db72ff3d7cd4"
-    sha256 catalina:      "bf75e06dc2472043c585f9df519c0f5b4506f5629436978ba22245b517ca2869"
+    sha256 arm64_ventura:  "39b3f6a5913c7532cf7b9abe759573a1d75370832dbf865a5d319fda54ee9513"
+    sha256 arm64_monterey: "0c20819875be1bcd53c04c80bbf3b42b6230f64690faaf7ed69abda5c21156ac"
+    sha256 arm64_big_sur:  "3c8fcab1cdc8dce5a1de2bad8d6ace5a1829d0a64840f9279770b14afef520e0"
+    sha256 ventura:        "d9ad7ba4f4a971ad5c7e9e04cf981421f9a2b014cb591b30687074e0330e5b3b"
+    sha256 monterey:       "258c55480e0ca4e72542ad071eb9cac590740a888ad485a427a3af83608b1aec"
+    sha256 big_sur:        "445fed7d16e95c6de6e28e01c991cd24ffc0189bba1f02607a99438bb87a6a0b"
+    sha256 x86_64_linux:   "610671eea58cf45d0bef507d18477ad505ff3fc4c7425386750b1ab5fac732fd"
   end
 
-  # All this formula does is disable COCOA which leads to:
-  # Class GNotificationCenterDelegate is implemented in both rsvg, magick
-
+  depends_on "gettext" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "gettext"
-  depends_on "libffi"
-  depends_on "pcre"
-  depends_on "python@3.9"
+  depends_on "pcre2"
+
+  uses_from_macos "libffi", since: :catalina
+  uses_from_macos "python", since: :catalina
+
+  on_macos do
+    depends_on "gettext"
+  end
 
   on_linux do
     depends_on "util-linux"
   end
 
-  # https://bugzilla.gnome.org/show_bug.cgi?id=673135 Resolved as wontfix,
-  # but needed to fix an assumption about the location of the d-bus machine
-  # id file.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/6164294a75541c278f3863b111791376caa3ad26/glib/hardcoded-paths.diff"
-    sha256 "a57fec9e85758896ff5ec1ad483050651b59b7b77e0217459ea650704b7d422b"
-  end
+  # These used to live in the now defunct `glib-utils`.
+  link_overwrite "bin/gdbus-codegen", "bin/glib-genmarshal", "bin/glib-mkenums", "bin/gtester-report"
+  link_overwrite "share/glib-2.0/codegen", "share/glib-2.0/gdb"
 
-  # required for gtk4
-  # see discussion at https://gitlab.gnome.org/GNOME/gtk/-/issues/3477
+  # replace several hardcoded paths with homebrew counterparts
   patch do
-    url "https://gitlab.gnome.org/GNOME/glib/-/commit/8c76bec77985be7f4c81a052ec649232341369f6.diff"
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/43467fd8dfc0e8954892ecc08fab131242dca025/glib/hardcoded-paths.diff"
+    sha256 "d81c9e8296ec5b53b4ead6917f174b06026eeb0c671dfffc4965b2271fb6a82c"
   end
 
   def install
-    inreplace %w[gio/gdbusprivate.c gio/xdgmime/xdgmime.c glib/gutils.c],
-      "@@HOMEBREW_PREFIX@@", HOMEBREW_PREFIX
+    inreplace %w[gio/xdgmime/xdgmime.c glib/gutils.c], "@@HOMEBREW_PREFIX@@", HOMEBREW_PREFIX
+    # Avoid the sandbox violation when an empty directory is created outside of the formula prefix.
+    inreplace "gio/meson.build", "install_emptydir(glib_giomodulesdir)", ""
 
+    # Jeroen: All this formula does is disable COCOA which leads to:
+    # Class GNotificationCenterDelegate is implemented in both rsvg, magick
     inreplace "meson.build", "<Cocoa/Cocoa.h>", "<blablabla.str>"
+    inreplace "gio/giomodule.c", "g_type_ensure (g_cocoa_notification_backend_get_type ());", ""
 
     # Disable dtrace; see https://trac.macports.org/ticket/30413
-    args = std_meson_args + %W[
+    # and https://gitlab.gnome.org/GNOME/glib/-/issues/653
+    args = %W[
       --default-library=both
-      -Diconv=auto
+      --localstatedir=#{var}
       -Dgio_module_dir=#{HOMEBREW_PREFIX}/lib/gio/modules
       -Dbsymbolic_functions=false
       -Ddtrace=false
+      -Druntime_dir=#{var}/run
     ]
 
-    mkdir "build" do
-      system "meson", *args, ".."
-      system "ninja", "-v"
-      system "ninja", "install", "-v"
-      bin.find { |f| rewrite_shebang detected_python_shebang, f }
-    end
+    system "meson", "setup", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
 
     # ensure giomoduledir contains prefix, as this pkgconfig variable will be
     # used by glib-networking and glib-openssl to determine where to install
@@ -76,26 +76,32 @@ class GlibLite < Formula
               "giomoduledir=#{HOMEBREW_PREFIX}/lib/gio/modules",
               "giomoduledir=${libdir}/gio/modules"
 
-    # `pkg-config --libs glib-2.0` includes -lintl, and gettext itself does not
-    # have a pkgconfig file, so we add gettext lib and include paths here.
-    gettext = Formula["gettext"].opt_prefix
-    inreplace lib+"pkgconfig/glib-2.0.pc" do |s|
-      s.gsub! "Libs: -L${libdir} -lglib-2.0 -lintl",
-              "Libs: -L${libdir} -lglib-2.0 -L#{gettext}/lib -lintl"
-      s.gsub! "Cflags: -I${includedir}/glib-2.0 -I${libdir}/glib-2.0/include",
-              "Cflags: -I${includedir}/glib-2.0 -I${libdir}/glib-2.0/include -I#{gettext}/include"
+    if OS.mac?
+      # `pkg-config --libs glib-2.0` includes -lintl, and gettext itself does not
+      # have a pkgconfig file, so we add gettext lib and include paths here.
+      gettext = Formula["gettext"].opt_prefix
+      inreplace lib/"pkgconfig/glib-2.0.pc" do |s|
+        s.gsub! "Libs: -L${libdir} -lglib-2.0 -lintl",
+                "Libs: -L${libdir} -lglib-2.0 -L#{gettext}/lib -lintl"
+        s.gsub! "Cflags: -I${includedir}/glib-2.0 -I${libdir}/glib-2.0/include",
+                "Cflags: -I${includedir}/glib-2.0 -I${libdir}/glib-2.0/include -I#{gettext}/include"
+      end
     end
 
-    # `pkg-config --print-requires-private gobject-2.0` includes libffi,
-    # but that package is keg-only so it needs to look for the pkgconfig file
-    # in libffi's opt path.
-    libffi = Formula["libffi"].opt_prefix
-    inreplace lib+"pkgconfig/gobject-2.0.pc" do |s|
-      s.gsub! "Requires.private: libffi",
-              "Requires.private: #{libffi}/lib/pkgconfig/libffi.pc"
+    if MacOS.version < :catalina
+      # `pkg-config --print-requires-private gobject-2.0` includes libffi,
+      # but that package is keg-only so it needs to look for the pkgconfig file
+      # in libffi's opt path.
+      libffi = Formula["libffi"].opt_prefix
+      inreplace lib/"pkgconfig/gobject-2.0.pc" do |s|
+        s.gsub! "Requires.private: libffi",
+                "Requires.private: #{libffi}/lib/pkgconfig/libffi.pc"
+      end
     end
 
-    bash_completion.install Dir["gio/completion/*"]
+    rm "gio/completion/.gitignore"
+    bash_completion.install (buildpath/"gio/completion").children
+    rewrite_shebang detected_python_shebang(use_python_from_path: true), *bin.children
   end
 
   def post_install
@@ -106,15 +112,12 @@ class GlibLite < Formula
     (testpath/"test.c").write <<~EOS
       #include <string.h>
       #include <glib.h>
-
       int main(void)
       {
           gchar *result_1, *result_2;
           char *str = "string";
-
           result_1 = g_convert(str, strlen(str), "ASCII", "UTF-8", NULL, NULL, NULL);
           result_2 = g_convert(result_1, strlen(result_1), "UTF-8", "ASCII", NULL, NULL, NULL);
-
           return (strcmp(str, result_2) == 0) ? 0 : 1;
       }
     EOS
