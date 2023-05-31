4fa06509702130fc13afef15f0df1e10a3f2d0a1
Jeroen Ooms <jeroenooms@gmail.com>
2023-05-31 22:28:36 +0200

    enable qr decoder

--- Formula/opencv-static.rb
+++ Formula/opencv-static.rb
@@ -4,6 +4,7 @@ class OpencvStatic < Formula
   url "https://github.com/opencv/opencv/archive/refs/tags/4.7.0.tar.gz"
   sha256 "8df0079cdbe179748a18d44731af62a245a45ebf5085223dc03133954c662973"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url :stable
@@ -86,6 +87,7 @@ class OpencvStatic < Formula
       -DWITH_QUIRC=OFF
       -DWITH_ADE=OFF
       -DWITH_IPP=OFF
+      -DWITH_QUIRC=ON
       -DBUILD_opencv_python2=OFF
       -DBUILD_opencv_python3=OFF
     ]
