ae2282a850e783ce01ad6174962f73ddf71915c6
Jeroen Ooms <jeroenooms@gmail.com>
2023-08-05 12:49:01 +0200

    add ventura bottle

--- Formula/aws-sdk-cpp-static.rb
+++ Formula/aws-sdk-cpp-static.rb
@@ -9,6 +9,7 @@ class AwsSdkCppStatic < Formula
   bottle do
     root_url "https://github.com/autobrew/homebrew-cran/releases/download/aws-sdk-cpp-static-1.9.163"
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "2a1ecb1a4165569bbc2d8ac77f44bdb7afb94f21767be47e3411c949a76abb6b"
+    sha256 cellar: :any_skip_relocation, ventura:       "78c65c772a69903337f0b3d674c65f2df739e43428b7f9d9d73232ae3da5807c"
     sha256 cellar: :any_skip_relocation, big_sur:       "a341ae23be8bfbd70125219ff8bc52e71e6c22dbd5b630808c6bac0088ffac4e"
     sha256 cellar: :any_skip_relocation, catalina:      "4f6586822f8b61083a4dcec09a550a2bb2e018dc5804f6d84686d57e6123feaa"
     sha256 cellar: :any_skip_relocation, monterey:      "7e12a0802b447ac7b3c5ee22c0ab8f362212d1cfd1e8ee613014dfba1b6224c7"
