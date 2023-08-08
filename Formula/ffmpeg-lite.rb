3fb436cd0a12178edf80c92c40b30b0dd7675e79
Jeroen Ooms <jeroenooms@gmail.com>
2023-08-08 10:55:25 +0200

    Bump ffmpeg

--- Formula/ffmpeg-lite.rb
+++ Formula/ffmpeg-lite.rb
@@ -1,12 +1,21 @@
 class FfmpegLite < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-5.1.2.tar.xz"
-  sha256 "619e706d662c8420859832ddc259cd4d4096a48a2ce1eefd052db9e440eef3dc"
   # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
   # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   license "GPL-2.0-or-later"
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
+
+  stable do
+    url "https://ffmpeg.org/releases/ffmpeg-6.0.tar.xz"
+    sha256 "57be87c22d9b49c112b6d24bc67d42508660e6b718b3db89c44e47e289137082"
+
+    # Fix bug in 6.0.0 with enable-hardcoded-tables
+    patch do
+      url "https://github.com/FFmpeg/FFmpeg/commit/814178f92647be2411516bbb82f48532373d2554.patch?full_index=1"
+      sha256 "5e4e17e76d9fb045035064bed76812371136cbf3c86d2ddcaa98bd7c542cfcfa"
+    end
+  end
 
   livecheck do
     url "https://ffmpeg.org/download.html"
@@ -58,7 +67,7 @@ class FfmpegLite < Formula
       --disable-indev=jack
     ]
 
-    args << "--enable-videotoolbox" if OS.mac?
+    args += %w[--enable-videotoolbox --enable-audiotoolbox] if OS.mac?
 
     system "./configure", *args
     system "make", "install"
