5f1fb5c4ee4ab04430d742920a85facbc4b44cc6
Jeroen Ooms <jeroen@berkeley.edu>
2023-09-03 12:10:22 +0000

    v8-static: update 11.7.439.14 bottle.

--- Formula/v8-static.rb
+++ Formula/v8-static.rb
@@ -12,11 +12,10 @@ class V8Static < Formula
   end
 
   bottle do
-    root_url "https://github.com/autobrew/homebrew-cran/releases/download/v8-static-11.5.150.22"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e1bc1dea9a052a310bc4575caa5c2bf76dedcf6163782bf3de5a31f34e4f92b2"
-    sha256 cellar: :any_skip_relocation, ventura:       "4f8196457107196c65d6fe78deeb96153d0927f93f4c7fa64297a91ccc027caf"
-    sha256 cellar: :any_skip_relocation, monterey:      "8deb84f1b331704c7fe0d9966a70df41ad4727a3345c00b1e3527035fd878531"
-    sha256 cellar: :any_skip_relocation, big_sur:       "e24269876e7c68bb8bbc54954d41213559115cc8ac881a47920afc77ee28d3cd"
+    root_url "https://github.com/autobrew/homebrew-cran/releases/download/v8-static-11.7.439.14"
+    sha256 cellar: :any_skip_relocation, ventura:  "a373e84707429869026bcddb9b5e9186b76a019fe9da2604fc02c3d412bc450d"
+    sha256 cellar: :any_skip_relocation, monterey: "6024fe33f3c82a3a919c63a3029f7db9adc2902ac7e1752237aef9b45a8c9424"
+    sha256 cellar: :any_skip_relocation, big_sur:  "3edb1292162ef7101ea0232d82e75fe56045b3df63137fdd9c62ae345da179a7"
   end
 
   depends_on "ninja" => :build
