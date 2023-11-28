6dfebfe690924a86b666a353444f7180eea418af
Gábor Csárdi <csardi.gabor@gmail.com>
2023-11-28 13:13:21 +0100

    Add static grpc

---
+++ Formula/grpc-static.rb
@@ -0,0 +1,118 @@
+class GrpcStatic < Formula
+  desc "Next generation open source RPC library and framework"
+  homepage "https://grpc.io/"
+  url "https://github.com/grpc/grpc.git",
+      tag:      "v1.59.3",
+      revision: "35df344f5e17a9cb290ebf0f5b0f03ddb1ff0a97"
+  license "Apache-2.0"
+  revision 1
+  head "https://github.com/grpc/grpc.git", branch: "master"
+
+  # There can be a notable gap between when a version is tagged and a
+  # corresponding release is created, so we check releases instead of the Git
+  # tags. Upstream maintains multiple major/minor versions and the "latest"
+  # release may be for an older version, so we have to check multiple releases
+  # to identify the highest version.
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :github_releases
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "34803fa1fb81a79dace2c02ce4047ef9a75398fa2a14cd9471dac50e76811feb"
+    sha256 cellar: :any,                 arm64_ventura:  "6b1a2bd1560e80d9fa2e5472f364c7fc2f86433ef8ad8209d3605378646e1c2c"
+    sha256 cellar: :any,                 arm64_monterey: "277b7b02c2d1fa17849ac6112d3f2d5c8d66ae5e419bcb1cf3facd3a68c5258a"
+    sha256 cellar: :any,                 sonoma:         "822e823b454095607251ced58a26055d5594deef27a925e5ac4e96c13ac8f602"
+    sha256 cellar: :any,                 ventura:        "f60412704844ce6266db055ed4b8ebcb87734ba5d491a7a500e6d64215648949"
+    sha256 cellar: :any,                 monterey:       "a1a19e2a4b6d6691f84606ac4d853deae4125966e82910e53301ca90b73ff144"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "75200ec6526d95edce0aa898a7c4078af480736b7d8adf6b7681dc991dd3ac9e"
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "cmake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :test
+  depends_on "abseil-static"
+  depends_on "c-ares-static"
+  # this is temporary
+  depends_on "gaborcsardi/cran/openssl-static"
+  depends_on "gaborcsardi/cran/protobuf-static"
+  depends_on "re2-static"
+
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1100
+  end
+
+  fails_with :clang do
+    build 1100
+    cause "Requires C++17 features not yet implemented"
+  end
+
+  fails_with gcc: "5" # C++17
+
+  def install
+    ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1100)
+    mkdir "cmake/build" do
+      args = %W[
+        ../..
+        -DCMAKE_CXX_STANDARD=17
+        -DCMAKE_CXX_STANDARD_REQUIRED=TRUE
+        -DCMAKE_INSTALL_RPATH=#{rpath}
+        -DBUILD_SHARED_LIBS=OFF
+        -DBUILD_STATIC_LIBS=ON
+        -DgRPC_BUILD_TESTS=OFF
+        -DgRPC_INSTALL=ON
+        -DgRPC_ABSL_PROVIDER=package
+        -DgRPC_CARES_PROVIDER=package
+        -DgRPC_PROTOBUF_PROVIDER=package
+        -DgRPC_SSL_PROVIDER=package
+        -DgRPC_ZLIB_PROVIDER=package
+        -DgRPC_RE2_PROVIDER=package
+      ] + std_cmake_args
+
+      system "cmake", *args
+      system "make", "install"
+
+      args = %W[
+        ../..
+        -DCMAKE_INSTALL_RPATH=#{rpath}
+        -DBUILD_SHARED_LIBS=OFF
+        -DBUILD_STATIC_LIBS=ON
+        -DgRPC_BUILD_TESTS=ON
+      ] + std_cmake_args
+      ENV["CFLAGS"] = "-mmacosx-version-min=11.0"
+      ENV["CXXFLAGS"] = "-mmacosx-version-min=11.0"
+      ENV["LDFLAGS"] = "-mmacosx-version-min=11.0"
+      system "cmake", *args
+      system "make", "grpc_cli"
+      bin.install "grpc_cli"
+
+      if OS.mac?
+        # These are installed manually, so need to be relocated manually as well
+        MachO::Tools.add_rpath(bin/"grpc_cli", rpath)
+      end
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <grpc/grpc.h>
+      int main() {
+        grpc_init();
+        grpc_shutdown();
+        return GRPC_STATUS_OK;
+      }
+    EOS
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["openssl@3"].opt_lib/"pkgconfig"
+    pkg_config_flags = shell_output("pkg-config --cflags --libs libcares protobuf re2 grpc++").chomp.split
+    system ENV.cc, "test.cpp", "-L#{Formula["abseil"].opt_lib}", *pkg_config_flags, "-o", "test"
+    system "./test"
+
+    output = shell_output("#{bin}/grpc_cli ls localhost:#{free_port} 2>&1", 1)
+    assert_match "Received an error when querying services endpoint.", output
+  end
+end
