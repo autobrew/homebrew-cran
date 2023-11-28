56078835543816ed579b69ec07cbe7b504953706
Gábor Csárdi <csardi.gabor@gmail.com>
2023-11-28 13:58:28 +0100

    Update protobuf

--- Formula/protobuf-static.rb
+++ Formula/protobuf-static.rb
@@ -1,19 +1,9 @@
 class ProtobufStatic < Formula
   desc "Protocol buffers (Google's data interchange format)"
-  homepage "https://github.com/protocolbuffers/protobuf/"
+  homepage "https://protobuf.dev/"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v25.1/protobuf-25.1.tar.gz"
+  sha256 "9bd87b8280ef720d3240514f884e56a712f2218f0d693b48050c836028940a42"
   license "BSD-3-Clause"
-  head "https://github.com/protocolbuffers/protobuf.git", branch: "main"
-
-  stable do
-    url "https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protobuf-all-21.12.tar.gz"
-    sha256 "2c6a36c7b5a55accae063667ef3c55f2642e67476d96d355ff0acb13dbb47f09"
-
-    # Fix build with Python 3.11. Remove in the next release.
-    patch do
-      url "https://github.com/protocolbuffers/protobuf/commit/da973aff2adab60a9e516d3202c111dbdde1a50f.patch?full_index=1"
-      sha256 "911925e427a396fa5e54354db8324c0178f5c602b3f819f7d471bb569cc34f53"
-    end
-  end
 
   livecheck do
     url :stable
@@ -21,15 +11,22 @@ class ProtobufStatic < Formula
   end
 
   bottle do
-    root_url "https://github.com/autobrew/homebrew-cran/releases/download/protobuf-static-21.12"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "a64a86e937912c5f47289e1eb741611cd2a45e265d60e149a952d1da3165a699"
-    sha256 cellar: :any_skip_relocation, monterey:      "b3510fbd4df29e0da0e919ca8cc44e6dd1104ce61def72821684a627a6ba6d67"
-    sha256 cellar: :any_skip_relocation, big_sur:       "13a90333e98e16863035bd63e31b2519a1c3a22ab540a8844ce990f047d4ddee"
+    sha256                               arm64_sonoma:   "6e19f8133d388f868b407a7eb9574fa3ec1a80a3831f4e367977e475a07bf280"
+    sha256                               arm64_ventura:  "71951995260eca6b9474e468372d13be43cc172b579ec48422d3f0a991b3f752"
+    sha256                               arm64_monterey: "3615e7f2f90ffc582edec2319a0fc09a65d13464cd794dcc8194abce6dca53e1"
+    sha256                               sonoma:         "4053a1e8a6be1520a17193bc7761849d583f7176f7efdd37252568ea7e0051c2"
+    sha256                               ventura:        "d518fa7501f1696b517b17732a874a93565dd775d538c1feff03a6b9c8c7dba2"
+    sha256                               monterey:       "ab0a5867b35eeafd4b045795ed2432aa3a04c6f33659d97a33758f52a274176f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "103b44b7ab5af979e5289e98b729f5e57161c7e81d19837bc25cf5df79100890"
   end
 
   depends_on "cmake" => :build
+  depends_on "python-setuptools" => :build
   depends_on "python@3.10" => [:build, :test]
   depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "abseil-static"
+  depends_on "jsoncpp-static"
 
   uses_from_macos "zlib"
 
@@ -39,19 +36,30 @@ class ProtobufStatic < Formula
         .map { |f| f.opt_libexec/"bin/python" }
   end
 
-  # Jeroen: make sure protoc executable is only statically linked
   def install
+    # Keep `CMAKE_CXX_STANDARD` in sync with the same variable in `abseil.rb`.
+    abseil_cxx_standard = 17
+    ENV["CFLAGS"] = "-mmacosx-version-min=11.0"
+    ENV["CXXFLAGS"] = "-mmacosx-version-min=11.0"
+    ENV["LDFLAGS"] = "-mmacosx-version-min=11.0"
     cmake_args = %w[
+      -DBUILD_SHARED_LIBS=OFF
+      -DBUILD_STATIC_LIBS=ON
       -Dprotobuf_BUILD_LIBPROTOC=ON
+      -Dprotobuf_BUILD_SHARED_LIBS=OFF
+      -Dprotobuf_BUILD_STATIC_LIBS=ON
       -Dprotobuf_INSTALL_EXAMPLES=ON
       -Dprotobuf_BUILD_TESTS=OFF
-    ] + std_cmake_args
+      -Dprotobuf_ABSL_PROVIDER=package
+      -Dprotobuf_JSONCPP_PROVIDER=package
+    ]
+    cmake_args << "-DCMAKE_CXX_STANDARD=#{abseil_cxx_standard}"
 
-    system "cmake", "-S", ".", "-B", "build", "-Dprotobuf_BUILD_SHARED_LIBS=OFF", *cmake_args
+    system "cmake", "-S", ".", "-B", "build", *cmake_args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
-    pkgshare.install "editors/proto.vim"
+    (share/"vim/vimfiles/syntax").install "editors/proto.vim"
     elisp.install "editors/protobuf-mode.el"
 
     ENV.append_to_cflags "-I#{include}"
@@ -59,9 +67,13 @@ class ProtobufStatic < Formula
     ENV["PROTOC"] = bin/"protoc"
 
     cd "python" do
+      # Keep C++ standard in sync with `abseil.rb`.
+      inreplace "setup.py", "extra_compile_args.append('-std=c++14')",
+                            "extra_compile_args.append('-std=c++#{abseil_cxx_standard}')"
+
       pythons.each do |python|
         pyext_dir = prefix/Language::Python.site_packages(python)/"google/protobuf/pyext"
-        with_env(LDFLAGS: "-Wl,-rpath,#{rpath(source: pyext_dir)} #{ENV.ldflags}") do
+        with_env(LDFLAGS: "-Wl,-rpath,#{rpath(source: pyext_dir)} #{ENV.ldflags}".strip) do
           system python, *Language::Python.setup_install_args(prefix, python), "--cpp_implementation"
         end
       end
@@ -83,7 +95,7 @@ class ProtobufStatic < Formula
     system bin/"protoc", "test.proto", "--cpp_out=."
 
     pythons.each do |python|
-      system python, "-c", "import google.protobuf"
+      system python, "-c", "from google.protobuf.pyext import _message"
     end
   end
 end
