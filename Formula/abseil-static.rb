5686023c218bdd98aa350cb9a5f4363d86e33439
Jeroen Ooms <jeroen@berkeley.edu>
2023-12-07 11:50:08 +0000

    abseil-static: update 20220623.1 bottle.

--- Formula/abseil-static.rb
+++ Formula/abseil-static.rb
@@ -7,15 +7,10 @@ class AbseilStatic < Formula
   head "https://github.com/abseil/abseil-cpp.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_ventura:  "da860134f8c93154324c1eb7ceac85224c8701e2a1758988ac9077778bebaa34"
-    sha256 cellar: :any,                 arm64_monterey: "a684dd51320207ef4cd8134ab8e7e033fba14a7dc1d9f1c8cb1c4b770dc715ff"
-    sha256 cellar: :any,                 arm64_big_sur:  "d31162eae71f007c10329296c0a646d6223d0f73e52d5fd8e2fc8333091ba377"
-    sha256 cellar: :any,                 ventura:        "6733be2ec57587f9051b357999faa62a96c55fd4df75b6349a119a6e76bfc884"
-    sha256 cellar: :any,                 monterey:       "41a3b0ca19070a90158beb85965e5485f116ad0e47b6c16ba9197c4324e5eab2"
-    sha256 cellar: :any,                 big_sur:        "80a398e384146b1860a79d0ff6c0666de74aca714fef357b0cadf525bae2a289"
-    sha256 cellar: :any,                 catalina:       "731fbe6746ab703ce4f6d17862de7ddb216b308d9d3948112c87ac0bfeb66d64"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f35a7844efa0e3862361a1bbff49e4f65a1d5eb7bc1d77c82a526419eadc6d2d"
+    root_url "https://github.com/autobrew/homebrew-cran/releases/download/abseil-static-20220623.1"
+    sha256 cellar: :any, ventura:  "8d2075e672a1a2cc80cf581bb995e060356a1a122a47df8e44f269a007094acd"
+    sha256 cellar: :any, monterey: "456f5a844fc48a32f84129a4422178ea44d8eec85b3f1efb33913c722e6850a3"
+    sha256 cellar: :any, big_sur:  "b7eb4a5fb4f12fa8cdefa291a74d32130299b6e4b674f4e760101ad01c6cb311"
   end
 
   depends_on "cmake" => :build
