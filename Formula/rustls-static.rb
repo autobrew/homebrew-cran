d82740c05425ca07487676c2ccc2532193e131b1
Jeroen Ooms <jeroenooms@gmail.com>
2025-05-23 21:58:10 +0200

    add formula

---
+++ Formula/rustls-static.rb
@@ -0,0 +1,36 @@
+class RustlsStatic < Formula
+  desc "FFI bindings for the rustls TLS library"
+  homepage "https://github.com/rustls/rustls-ffi"
+  url "https://github.com/rustls/rustls-ffi/archive/refs/tags/v0.15.0.tar.gz"
+  sha256 "db3939a58677e52f03603b332e00347b29aa57aa4012b5f8a7e779ba2934b18b"
+  license any_of: ["Apache-2.0", "MIT", "ISC"]
+  head "https://github.com/rustls/rustls-ffi.git", branch: "main"
+
+  depends_on "cargo-c" => :build
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+
+  def install
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = "11.0"
+    system "cargo", "cinstall", "--jobs", ENV.make_jobs.to_s, "--release", "--prefix", prefix, "--libdir", lib
+  end
+
+  test do
+    (testpath/"test-rustls.c").write <<~C
+      #include "rustls.h"
+      #include <stdio.h>
+      int main(void) {
+        struct rustls_str version = rustls_version();
+        printf("%s", version.data);
+        return 0;
+      }
+    C
+
+    ENV.append_to_cflags "-I#{include}"
+    ENV.append "LDFLAGS", "-L#{lib}"
+    ENV.append "LDLIBS", "-lrustls"
+
+    system "make", "test-rustls"
+    assert_match version.to_s, shell_output("./test-rustls")
+  end
+end
