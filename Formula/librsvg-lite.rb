e550c8ac1484e70b53b43bbd2729001aa8b0395b
Jeroen Ooms <jeroenooms@gmail.com>
2025-09-06 12:10:00 +0200

    librsvg without fontconfig
    Closes #155.
    
    Signed-off-by: Jeroen Ooms <216319+jeroen@users.noreply.github.com>

---
+++ Formula/librsvg-lite.rb
@@ -0,0 +1,92 @@
+class LibrsvgLite < Formula
+  desc "Library to render SVG files using Cairo"
+  homepage "https://wiki.gnome.org/Projects/LibRsvg"
+  url "https://download.gnome.org/sources/librsvg/2.61/librsvg-2.61.0.tar.xz"
+  sha256 "dbd0db40a1179a382fbb8cc930837671b973d722ba106a3dee2aad0fd858e2c4"
+  license "LGPL-2.1-or-later"
+
+  # librsvg doesn't use GNOME's "even-numbered minor is stable" version scheme.
+  # This regex matches any version that doesn't have a 90+ patch version, as
+  # those are development releases.
+  livecheck do
+    url :stable
+    regex(/librsvg[._-]v?(\d+\.\d+\.(?:\d|[1-8]\d+)(?:\.\d+)*)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any, arm64_sequoia: "ffb4e691e2c3d62decf0e59349bae5386a900ef7b6c9b68cbb8c1d9f2a806b4e"
+    sha256 cellar: :any, arm64_sonoma:  "4d6857350f453cee62797559cce3568060b3dea1495f449d2763cdc007d07b9c"
+    sha256 cellar: :any, arm64_ventura: "a6f2fc2ed73049f6340feb9462694b8e7e2b6dbd65d36175a7be4e190af6480e"
+    sha256 cellar: :any, sonoma:        "dc755722074cc389f23a2bcdff100c83f91675eef2229e92a4851dedb37f0021"
+    sha256 cellar: :any, ventura:       "129b360b516863ba5f207e22255d101eddfd54e8e102b1f72ed35c1cc937b56b"
+    sha256               arm64_linux:   "87b834095b43615cb95eab9312865385914dbede3a2576ab309084706c89111b"
+    sha256               x86_64_linux:  "f5552c43775e141cc03099e635cb867f5f436670798430333761879a1bb07da7"
+  end
+
+  depends_on "cargo-c" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => [:build, :test]
+  depends_on "rust" => :build
+  depends_on "cairo"
+  depends_on "gdk-pixbuf"
+  depends_on "glib"
+  depends_on "pango"
+
+  uses_from_macos "libxml2"
+
+  on_macos do
+    depends_on "freetype"
+    depends_on "gettext"
+    depends_on "harfbuzz"
+    depends_on "libpng"
+  end
+
+  def install
+    # Set `RPATH` since `cargo-c` doesn't seem to.
+    rpath_flags = [rpath, rpath(source: lib/"gdk-pixbuf-2.0/2.10.0/loaders")].map { |rp| "-rpath,#{rp}" }
+    ENV.append_to_rustflags "--codegen link-args=-Wl,#{rpath_flags.join(",")}" if OS.mac?
+
+    # disable updating gdk-pixbuf cache, we will do this manually in post_install
+    # https://github.com/Homebrew/homebrew/issues/40833
+    ENV["DESTDIR"] = "/"
+
+    system "meson", "setup", "build", "--default-library=both",
+                                      "-Dintrospection=enabled",
+                                      "-Dpixbuf=enabled",
+                                      "-Dpixbuf-loader=enabled",
+                                      *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+
+    # Workaround until https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1049
+    if OS.mac?
+      gdk_pixbuf_moduledir = lib.glob("gdk-pixbuf-*/*/loaders").first
+      gdk_pixbuf_modules = gdk_pixbuf_moduledir.glob("*.dylib")
+      odie "Try removing .so symlink workaround!" if gdk_pixbuf_modules.empty?
+      gdk_pixbuf_moduledir.install_symlink gdk_pixbuf_modules.to_h { |m| [m, m.sub_ext(".so").basename] }
+    end
+  end
+
+  def post_install
+    # librsvg is not aware GDK_PIXBUF_MODULEDIR must be set
+    # set GDK_PIXBUF_MODULEDIR and update loader cache
+    ENV["GDK_PIXBUF_MODULEDIR"] = "#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/2.10.0/loaders"
+    system "#{Formula["gdk-pixbuf"].opt_bin}/gdk-pixbuf-query-loaders", "--update-cache"
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      #include <librsvg/rsvg.h>
+
+      int main(int argc, char *argv[]) {
+        RsvgHandle *handle = rsvg_handle_new();
+        return 0;
+      }
+    C
+    flags = shell_output("pkgconf --cflags --libs librsvg-2.0").chomp.split
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
