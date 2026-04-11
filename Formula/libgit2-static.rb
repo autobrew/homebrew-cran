703d274720011c18cadf3f0bc847986c9bd29190
Jeroen Ooms <jeroenooms@gmail.com>
2026-04-11 15:46:01 +0200

    Update libgit2
    Closes #172.
    
    Signed-off-by: Jeroen Ooms <216319+jeroen@users.noreply.github.com>

--- Formula/libgit2-static.rb
+++ Formula/libgit2-static.rb
@@ -1,8 +1,8 @@
 class Libgit2Static < Formula
   desc "C library of Git core methods that is re-entrant and linkable"
-  homepage "https://libgit2.github.com/"
-  url "https://github.com/libgit2/libgit2/archive/refs/tags/v1.8.2.tar.gz"
-  sha256 "184699f0d9773f96eeeb5cb245ba2304400f5b74671f313240410f594c566a28"
+  homepage "https://libgit2.org/"
+  url "https://github.com/libgit2/libgit2/archive/refs/tags/v1.9.2.tar.gz"
+  sha256 "6f097c82fc06ece4f40539fb17e9d41baf1a5a2fc26b1b8562d21b89bc355fe6"
   license "GPL-2.0-only" => { with: "GCC-exception-2.0" }
   head "https://github.com/libgit2/libgit2.git", branch: "main"
 
